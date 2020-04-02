632b8493a9801ed50ecc0ce4e3ce91a1b3716f11
lapis1999 <lapis.1999@mail.kmutt.ac.th>
2020-04-03 00:31:40 +0700

    add cost api

---
+++ costapi.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const fs = require("fs"); //read file
+const mysql = require('mysql')
+
+app.get('/costTypeID', (req, res) => {
+    //console.log("costTypeID: " + req.params.id)
+
+    const connect = mysql.createConnection({
+        host: 'us-cdbr-iron-east-01.cleardb.net',
+        user: 'b30ea52d3aa952',
+        password: '918a5c13',
+        database: 'heroku_1c39a65d43a5546'
+    })
+    const queryString = "SELECT * FROM coststype"
+    connect.query(queryString,(err, rows, fields) => {
+        if(err){
+            console.log("Failed to query for users:" + err)
+            res.sendStatus(500)
+           return
+        }
+        console.log("fetch Success")
+
+        const costJson = rows.map((row)=>{
+        return {CostTypeID: ~~(row.CostTypeID/10)+1,CostTypeName: row.CostTypeName}
+        })
+        
+        res.json(costJson)
+    })
+    //res.end()
+});
+
+
+const server = app.listen(4200, function () {
+    const host = server.address().address
+    const port = server.address().port
+    console.log("server is listening on 4200")
+});
